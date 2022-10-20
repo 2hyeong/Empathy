d14e2cc84b12b0cf2f2e12d1c3eba4d66ffc0c0a
2hyeong <doo.hyeongnim@gmail.com>
2022-10-20 17:29:49 +0900

    chore: add primary main color on theme

--- packages/ui/Theme.tsx
+++ packages/ui/Theme.tsx
@@ -3,6 +3,9 @@ import { createTheme } from "@mui/material/styles";
 export const theme = createTheme({
   palette: {
     mode: "dark",
+    primary: {
+      main: "#202020",
+    },
   },
   typography: {
     fontFamily: [
