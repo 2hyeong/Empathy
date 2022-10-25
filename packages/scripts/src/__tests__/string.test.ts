a6ec3f22fcf46b40a9878b0c8844fe664c5de39f
2hyeong <doo.hyeongnim@gmail.com>
2022-10-25 22:59:34 +0900

    refactor: move into /src

---
+++ packages/scripts/src/__tests__/string.test.ts
@@ -0,0 +1,19 @@
+import { replaceAt } from "../string";
+
+describe("should return string replaced by given char", () => {
+  test("should return 'zbcd' if replaceAt('abcd', 0, 'z')  ", () => {
+    expect(replaceAt("abcd", 0, "z")).toBe("zbcd");
+  });
+
+  test("should return 'azcd' if replaceAt('test', 1, 'z')  ", () => {
+    expect(replaceAt("abcd", 1, "z")).toBe("azcd");
+  });
+
+  test("should return 'test' if replaceAt('test', 10, 'k')  ", () => {
+    expect(replaceAt("test", 10, "k")).toBe("test");
+  });
+
+  test("should return 'test' if replaceAt('test', -1, 'k')  ", () => {
+    expect(replaceAt("test", -1, "k")).toBe("test");
+  });
+});
