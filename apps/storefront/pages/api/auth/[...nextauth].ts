7087ccabb658262cd1aceebfafafbb72be853400
2hyeong <doo.hyeongnim@gmail.com>
2022-10-27 15:40:48 +0900

    chore: update next-auth adapter and add debug

--- apps/storefront/pages/api/auth/[...nextauth].ts
+++ apps/storefront/pages/api/auth/[...nextauth].ts
@@ -1,19 +1,26 @@
 import NextAuth from "next-auth";
 import NaverProvider from "next-auth/providers/naver";
 import KakaoProvider from "next-auth/providers/kakao";
-import { FirestoreAdapter } from "@next-auth/firebase-adapter"
-import { firebaseConfig, kakaoConfig, naverConfig } from "scripts"
-import { firebase } from "scripts";
+import { FirestoreAdapter } from "@next-auth/firebase-adapter";
+import { firebaseConfig, kakaoConfig, naverConfig } from "scripts";
+import { db } from "scripts/src/firebase-admin";
+
+const adapter = !db ? FirestoreAdapter(firebaseConfig) : undefined;
 
 export default NextAuth({
-    secret: process.env.NEXTAUTH_SECRET,
-    providers: [
-        NaverProvider(naverConfig),
-        KakaoProvider({...kakaoConfig, authorization: {
-            params: {
-                scope: "talk_message, age_range, gender, friends, birthday, account_email"
-            }
-        }}),
-    ],
-    adapter: firebase && FirestoreAdapter(firebaseConfig),
-})
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    NaverProvider(naverConfig),
+    KakaoProvider({
+      ...kakaoConfig,
+      authorization: {
+        params: {
+          scope:
+            "talk_message, age_range, gender, friends, birthday, account_email",
+        },
+      },
+    }),
+  ],
+  adapter: adapter,
+  debug: process.env.NODE_ENV === "development",
+});
