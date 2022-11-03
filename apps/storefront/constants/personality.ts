ea5cd68ef93c859d32f9cbdf61257b193209e5c2
2hyeong <doo.hyeongnim@gmail.com>
2022-11-03 12:41:54 +0900

    chore: add personality16

---
+++ apps/storefront/constants/personality.ts
@@ -0,0 +1,50 @@
+export const personalities16 = [
+  {
+    label: "ISTJ",
+  },
+  {
+    label: "ISFJ",
+  },
+  {
+    label: "INFJ",
+  },
+  {
+    label: "INTJ",
+  },
+  {
+    label: "ISTP",
+  },
+  {
+    label: "ISFP",
+  },
+  {
+    label: "INFP",
+  },
+  {
+    label: "INTP",
+  },
+  {
+    label: "ESTP",
+  },
+  {
+    label: "ESFP",
+  },
+  {
+    label: "ENFP",
+  },
+  {
+    label: "ENTP",
+  },
+  {
+    label: "ESTJ",
+  },
+  {
+    label: "ESFJ",
+  },
+  {
+    label: "ENFJ",
+  },
+  {
+    label: "ENTJ",
+  },
+];
