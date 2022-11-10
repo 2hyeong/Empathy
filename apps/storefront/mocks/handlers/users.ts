4ad1ddf10438402a34d991b736d4bbc00bb0c0d2
2hyeong <doo.hyeongnim@gmail.com>
2022-11-10 14:26:54 +0900

    feat: add mocks

--- apps/storefront/mocks/handlers/users.ts
+++ apps/storefront/mocks/handlers/users.ts
@@ -3,10 +3,40 @@ import { Friend } from "idl/gen/typescript-fetch";
 
 const friends = [
   {
+    id: "0",
     name: "홍길동",
     personality: "ISTP",
   },
 ];
+
+const users = [
+  {
+    id: "Y0y0GKFXDNuyX6OO5QHD",
+    birthday: "0118",
+    ageRange: "30~39",
+    image:
+      "http://k.kakaocdn.net/dn/ckXZBw/btrPDkVvdea/Pz0mY8MStAI5zlIDsCJ5t0/img_640x640.jpg",
+    gender: "male",
+    emailVerified: null,
+    name: "이두형",
+    email: "doohl2@kakao.com",
+    personality: "INTP",
+    friends: [{ name: "홍길동", personality: "INTP" }],
+  },
+];
+
+const me = {
+  name: "이두형",
+  personality: "",
+};
+
+export const getUsers = () =>
+  rest.get("api/users", (req, res, ctx) =>
+    res(ctx.status(200), ctx.json(users))
+  );
+
+export const getMe = () =>
+  rest.get("/api/me", (req, res, ctx) => res(ctx.status(200), ctx.json(me)));
 
 export const getFriends = (mockData?: Friend[]) =>
   rest.get("/api/users/friends", (req, res, ctx) => {
@@ -23,4 +53,9 @@ export const addFriends = () =>
     return res(ctx.status(200), ctx.json(newFriends));
   });
 
-export default [getFriends(), addFriends()];
+export const updateUsers = () =>
+  rest.put("/api/users", (req, res, ctx) =>
+    res(ctx.status(200), ctx.json({ ok: true }))
+  );
+
+export default [getFriends(), addFriends(), getUsers(), getMe(), updateUsers()];
