24db3a0595d6d6935cf121281ba695370b866316
2hyeong <doo.hyeongnim@gmail.com>
2022-11-11 10:51:27 +0900

    fix: update missing next/auth types

--- apps/storefront/types/next-auth.d.ts
+++ apps/storefront/types/next-auth.d.ts
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth";
+import { JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
   /**
@@ -13,5 +14,27 @@ declare module "next-auth" {
       birthday: string;
       ageRange: string;
     };
+  }
+  interface User {
+    id: string;
+    name: string;
+    image: string;
+    gender?: string;
+    birthday?: string;
+    ageRange?: string;
+  }
+  interface Profile {
+    kakao_account: {
+      gender: string;
+      birthday: string;
+      age_range: string;
+    };
+  }
+}
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    user: User;
   }
 }
