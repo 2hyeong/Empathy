7338f76b4ec557c59fca51e66676a4610259cbcf
2hyeong <doo.hyeongnim@gmail.com>
2022-12-11 22:43:06 +0900

    chore: add auth guard

--- apps/storefront/features/auth/AuthGuard.tsx
+++ apps/storefront/features/auth/AuthGuard.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { ReactNode } from "react";
-import { signIn, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
+// component
+import SignInModal from "storefront/components/auth/SignInModal";
+import { useModal } from "storefront/hooks/useModal";
 
 export interface AuthGuardProps {
   children: ReactNode;
@@ -10,7 +13,13 @@ export default function AuthGuard({ children }: AuthGuardProps) {
   const { status } = useSession();
 
   if (status === "unauthenticated") {
-    signIn("kakao");
+    const { visible, show, close } = useModal({ defaultVisible: true });
+    return (
+      <>
+        <SignInModal visible={visible} show={show} close={close} />
+        {children}
+      </>
+    );
   }
 
   return <>{children}</>;
