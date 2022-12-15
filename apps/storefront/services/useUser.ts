7362733cceab3fa0bfcdccf8ca9c3d86d6c8664f
2hyeong <doo.hyeongnim@gmail.com>
2022-12-15 17:50:53 +0900

    chore: rename

--- apps/storefront/services/useUser.ts
+++ apps/storefront/services/useUser.ts
@@ -1,5 +1,5 @@
 import { User, UserToJSON } from "idl/gen/typescript-fetch";
-import { usersApi, userApi } from "./typescript-fetch";
+import { usersApi, userApi } from "./typescriptFetch";
 
 export const getUser = () => usersApi.getUsers();
 export const getMe = () => userApi.getMe();
