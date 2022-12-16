47a10142b5232488acd204c5fe3bbbe8694d4d7b
2hyeong <doo.hyeongnim@gmail.com>
2022-12-16 14:27:27 +0900

    chore: add emotion-cache

--- apps/storefront/app/providers.tsx
+++ apps/storefront/app/providers.tsx
@@ -1,7 +1,11 @@
 "use client";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { RecoilRoot } from "recoil";
 import { SessionProvider } from "next-auth/react";
+import { useServerInsertedHTML } from "next/navigation";
+// @emotion
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
 // ui
 import ThemeProvider from "ui/theme";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -9,14 +13,31 @@ import CssBaseline from "@mui/material/CssBaseline";
 import DashboardLayout from "storefront/components/layout/dashboard";
 
 export default function Providers({ children }: { children: ReactNode }) {
+  const [cache] = useState(() => {
+    const cache = createCache({ key: "css" });
+    cache.compat = true;
+    return cache;
+  });
+
+  useServerInsertedHTML(() => (
+    <style
+      data-emotion={`${cache.key} ${Object.keys(cache.inserted).join(" ")}`}
+      dangerouslySetInnerHTML={{
+        __html: Object.values(cache.inserted).join(" "),
+      }}
+    />
+  ));
+
   return (
-    <SessionProvider refetchInterval={5 * 60}>
-      <RecoilRoot>
-        <ThemeProvider>
-          <CssBaseline />
-          <DashboardLayout>{children}</DashboardLayout>
-        </ThemeProvider>
-      </RecoilRoot>
-    </SessionProvider>
+    <CacheProvider value={cache}>
+      <ThemeProvider>
+        <SessionProvider refetchInterval={5 * 60}>
+          <RecoilRoot>
+            <CssBaseline />
+            <DashboardLayout>{children}</DashboardLayout>
+          </RecoilRoot>
+        </SessionProvider>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
