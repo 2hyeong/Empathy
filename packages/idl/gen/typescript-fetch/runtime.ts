9958a338db6f0568811d4333e8196c8ac74299e6
2hyeong <doo.hyeongnim@gmail.com>
2022-12-19 16:31:39 +0900

    feat: add prod url

--- packages/idl/gen/typescript-fetch/runtime.ts
+++ packages/idl/gen/typescript-fetch/runtime.ts
@@ -13,7 +13,7 @@
  */
 
 
-export const BASE_PATH = "http://localhost:3000/api".replace(/\/+$/, "");
+export const BASE_PATH = "https://empahty.vercel.app/api".replace(/\/+$/, "");
 
 export interface ConfigurationParameters {
     basePath?: string; // override base path
